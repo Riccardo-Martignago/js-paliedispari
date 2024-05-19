d0f3bf6d7906366d310f2e7a53163afbf84f678d
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-19 19:36:30 +0200

    Eseguito esercizio parola palindroma con una funzione che controlla se la parola inserita é palindroma

--- Palidroma/JS/script.js
+++ Palidroma/JS/script.js
@@ -1,1 +1,18 @@
-console.log('Hello!')
+const userWord = prompt('Inserisci una parola')
+
+function palindroma (word){
+    const arrWord = word.split('');
+    const reverseWord = arrWord.toReversed();
+    if (arrWord.toString() === reverseWord.toString()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+if (palindroma(userWord) == true){
+    console.log('La parola é palindroma')
+}
+else{
+    console.log('La parola non é palindroma')
+}
